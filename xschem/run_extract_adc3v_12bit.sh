6b7d906cd14150d36d23826b9a863dbc21ced8d9
Tim Edwards <tim@opencircuitdesign.com>
2025-02-25 17:36:26 -0500

    Revised the layout to make it a standalone block, as it was previously only used to verify an early version in frigate_analog which was comprised of separate CDAC and comparator.  The layout has been compacted somewhat, and pins have been moved to the cell boundary. Several pins have been re-routed to the closest side of the circuit. Fixed a missed reference to "dependencies" in the schematic extraction script, and re-ran and confirmed LVS.

--- xschem/run_extract_adc3v_12bit.sh
+++ xschem/run_extract_adc3v_12bit.sh
@@ -7,10 +7,10 @@ echo ${PDK:=sky130A} > /dev/null
 
 # Construct XSCHEM_LIBRARY_PATH
 tclstr="set lvs_netlist 1"
-tclstr="$tclstr ; append XSCHEM_LIBRARY_PATH :$PWD/../dependencies/sky130_ef_ip__ccomp3v/xschem"
-tclstr="$tclstr ; append XSCHEM_LIBRARY_PATH :$PWD/../dependencies/sky130_ef_ip__cdac3v_12bit/xschem"
-tclstr="$tclstr ; append XSCHEM_LIBRARY_PATH :$PWD/../dependencies/sky130_ef_ip__samplehold/xschem"
-tclstr="$tclstr ; append XSCHEM_LIBRARY_PATH :$PWD/../dependencies/sky130_ef_ip__cdac3v_12bit/dependencies/sky130_ef_ip__analog_switches/xschem"
+tclstr="$tclstr ; append XSCHEM_LIBRARY_PATH :$PWD/../ip/sky130_ef_ip__ccomp3v/xschem"
+tclstr="$tclstr ; append XSCHEM_LIBRARY_PATH :$PWD/../ip/sky130_ef_ip__cdac3v_12bit/xschem"
+tclstr="$tclstr ; append XSCHEM_LIBRARY_PATH :$PWD/../ip/sky130_ef_ip__samplehold/xschem"
+tclstr="$tclstr ; append XSCHEM_LIBRARY_PATH :$PWD/../ip/sky130_ef_ip__cdac3v_12bit/ip/sky130_ef_ip__analog_switches/xschem"
 
 xschem -n -s -r -x -q --tcl "$tclstr" --rcfile $PDK_ROOT/$PDK/libs.tech/xschem/xschemrc -o ../netlist/schematic -N $project.spice $project.sch
 
